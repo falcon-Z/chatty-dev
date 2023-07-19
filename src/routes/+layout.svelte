2f4e2ea2b967e46a7c507782ed4d85dceaa88767
sai madhan <saimadhan@outlook.in>
2023-07-19 19:23:42 +0530

    added tanstack query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+
 	import '../app.postcss';
+	import { client } from '$lib/utils/appwrite';
+
+	const queryClient = new QueryClient();
 </script>
 
-<main class="grid h-screen w-full place-items-center">
-	<slot />
-</main>
+<QueryClientProvider client={queryClient}>
+	<main class="grid h-screen w-full place-items-center">
+		<slot />
+	</main>
+</QueryClientProvider>
