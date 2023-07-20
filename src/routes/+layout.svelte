cf91dd3e4c2b252df1dd26d04120462b7b56d096
sai madhan <saimadhan@outlook.in>
2023-07-20 19:26:39 +0530

    removed tanstack

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,20 +2,8 @@
 	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
 
 	import '../app.postcss';
-	import { browser } from '$app/environment';
-	import type { LayoutData } from './$types';
-
-	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				enabled: browser
-			}
-		}
-	});
 </script>
 
-<QueryClientProvider client={queryClient}>
-	<main class="relative grid h-screen w-full place-items-center">
-		<slot />
-	</main>
-</QueryClientProvider>
+<main class="relative grid h-screen w-full place-items-center">
+	<slot />
+</main>
